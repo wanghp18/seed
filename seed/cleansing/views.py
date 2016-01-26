20fcfcaa4704fab77126b259675ffa344ce86661
Nicholas Serra <nicks@ydekproductions.com>
2016-01-25 19:01:04 -0500

    Labels back from cleansing endpoint

--- seed/cleansing/views.py
+++ seed/cleansing/views.py
@@ -5,13 +5,17 @@
 :author
 """
 import csv
+
 from django.contrib.auth.decorators import login_required
-from seed.utils.cache import get_cache_raw, get_cache
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+
 from seed.cleansing.models import Cleansing
-from seed.decorators import ajax_request
+from seed.data_importer.models import ImportFile
+from seed.decorators import ajax_request, get_prog_key
+from seed.lib.superperms.orgs.models import Organization as SuperOrganization, OrganizationUser
+from seed.models import StatusLabel
 from seed.utils.api import api_endpoint
-from seed.decorators import get_prog_key
+from seed.utils.cache import get_cache_raw, get_cache
 
 
 # TODO The API is returning on both a POST and GET. Make sure to authenticate.
@@ -22,23 +26,49 @@ from seed.decorators import get_prog_key
 @login_required
 def get_cleansing_results(request):
     """
-    Retrieve the details of the cleansing script.
+    Retrieve the details of the cleansing script. Also return any labels that
+    were created as a result of these cleansing errors.
     """
-
     import_file_id = request.GET.get('import_file_id')
+    organization_id = request.GET.get('organization_id')
+
+    # Confirm that the organization exists and the user is a member
+    try:
+        org = SuperOrganization.objects.get(pk=organization_id)
+        OrganizationUser.objects.get(organization=org, user=request.user)
+    except (SuperOrganization.DoesNotExist, OrganizationUser.DoesNotExist):
+        return HttpResponseForbidden()
+
+    # Confirm this import file exists and the file belongs to the organization.
+    try:
+        ImportFile.objects.get(pk=import_file_id,
+            import_record__super_organization=org)
+    except ImportFile.DoesNotExist:
+        return HttpResponseBadRequest
+
     cleansing_results = get_cache_raw(Cleansing.cache_key(import_file_id))
+    label_names = []
 
     # add in additional fields for view
     for i, row in enumerate(cleansing_results):
         for j, result in enumerate(row['cleansing_results']):
+
+            # Only return labels that are a product of an error.
+            if result['severity'] == 'error':
+                label_names.append(result['message'])
+
             if result['field'] in Cleansing.ASSESSOR_FIELDS_BY_COLUMN:
                 result['field'] = Cleansing.ASSESSOR_FIELDS_BY_COLUMN[result['field']]['title']
+
+    label_names = list(set(label_names))
+    labels = StatusLabel.objects.filter(name__in=label_names, super_organization=org)
 
     return {
         'status': 'success',
         'message': 'Cleansing complete',
         'progress': 100,
-        'data': cleansing_results
+        'data': cleansing_results,
+        'labels': [l.to_dict() for l in labels]
     }
 
 
